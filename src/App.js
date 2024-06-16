62e6efdbe50dd0e4b4731c0dd6b333eaa4af98bb
Thamirisalvalle <thamirisalvalle@gmail.com>
2024-06-16 17:53:11 -0300

    Criando a lógica para mostrar o ranking.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import GameScreen from './components/GameScreen'; // importação dos componentes
 
 function App() {
   // Definição de estados
   const [gameState, setGameState] = useState('start');
+  const [highScores, setHighScores] = useState([]);
+
+
+  useEffect (() => {
+    const storedScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    setHighScores(storeScores);
+  }, []);
 
   // Iniciar o Jogo
   const startGame = () => {
@@ -16,13 +23,32 @@ function App() {
   };
 
   const winGame = () => {
+    const endTime = 10 - timeLeft;
+    const newScore = { time: endTime, date: new Date().toLocaleString() };
+    const updatedScores = [...highScores, newScore].sort((a, b) => a.time - b.time).slice(0, 5);
+    setHighScores(updatedScores);
+    localStorage.setItem('highScores', JSON.stringify(updatedScores));
     setGameState('success');
-  }
+  };
 
   return (
     // Mostrar botão iniciar jogo e novo jogo, mostrar a tela do jogo e mensagem de game over ou a mensagem de acertou tudo
     <div className="App">
-      {gameState === 'start' && <button onClick={startGame}>Iniciar</button>}
+      {gameState === 'start' && (
+        <>        
+          <button onClick={startGame}>Iniciar</button>
+          <div>
+            <h2>Melhores Pontuações:</h2>
+            <ul>
+              {highScores.map((score, index) => (
+                <li key={index}>
+                  {score.time}s - {score.date}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
       {gameState === 'playing' && <GameScreen endGame={endGame} winGame={winGame} />}
       {gameState === 'gameover' && (
         <div>
